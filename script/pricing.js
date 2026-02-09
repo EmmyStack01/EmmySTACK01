066b85574087682e1c16942694322df0ae1b85c7
EmmyStack01 <wezuduemoih@gmail.com>
2026-02-08 23:15:15 -0800

    Delete script/pricing.js

--- script/pricing.js
+++
@@ -1,9 +0,0 @@
-function downloadPricing() {
-    const choice = confirm("Would you like the International (USD) pricing? \n\n(Cancel for Local NGN pricing)");
-    
-    if (choice) {
-        window.open('assets/Emmy STACK01 Dollar Modular Pricing.png', '_blank');
-    } else {
-        window.open('assets/Emmy STACK01 Naira Modular Pricing.png', '_blank');
-    }
-}
