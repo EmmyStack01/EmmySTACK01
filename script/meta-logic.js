c93c7027eb1f92469ce0763bec0b69afcf51be9f
EmmyStack01 <wezuduemoih@gmail.com>
2026-04-24 11:41:23 +0100

    Update hostname check for meta logic script

--- script/meta-logic.js
+++ script/meta-logic.js
@@ -1,5 +1,5 @@
  // 1. Tell Search Engines to ignore this domain
-  if (window.location.hostname === 'emmystack01.github.io') {
+  if (window.location.hostname === 'emmystack01.github.io/EmmySTACK01') {
     var meta = document.createElement('meta');
     meta.name = 'robots';
     meta.content = 'noindex, nofollow';
@@ -7,6 +7,6 @@
 
     // 2. Redirect Users to the official .com domain
     // This script maintains the path (e.g., /partnership stays /partnership)
-    var newLocation = window.location.href.replace('emmystack01.github.io', 'emmystack01.com');
+    var newLocation = window.location.href.replace('emmystack01.github.io/EmmySTACK01', 'emmystack01.com');
     window.location.replace(newLocation);
   }
