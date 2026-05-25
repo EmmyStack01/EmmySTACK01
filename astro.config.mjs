ca7653426600104a6a58c9bc11e22f5484e48526
EmmyStack01 <wezuduemoih@gmail.com>
2026-05-25 11:43:21 -0700

    indexnow updates and multiple h1 fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import keystatic from '@keystatic/astro';
 import vercel from "@astrojs/vercel";
 import react from '@astrojs/react';
+import indexnow from 'astro-indexnow';
 
 export default defineConfig({
   site: 'https://emmystack01.com',
@@ -28,6 +29,12 @@ export default defineConfig({
         !page.includes('/payment') &&
         !page.includes('/preview')
     }), 
-    react()
+    react(),
+    indexnow({
+      site: 'https://emmystack01.com',
+      key: 'ec64748f40c8436da57b45e04b431172',
+      // Optional: points the engines to your hosted key file if needed
+      keyLocation: 'https://emmystack01.com/ec64748f40c8436da57b45e04b431172.txt'
+    })
   ],
 });
