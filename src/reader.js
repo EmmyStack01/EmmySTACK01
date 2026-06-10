8f73d703562cbea99242e48fc591ac1a38405b92
EmmyStack01 <wezuduemoih@gmail.com>
2026-06-10 00:52:23 -0700

    fix reader for production

--- src/reader.js
+++ src/reader.js
@@ -1,4 +1,12 @@
 import { createReader } from '@keystatic/core/reader';
+import { createGitHubReader } from '@keystatic/core/reader/github';
 import keystaticConfig from '../keystatic.config';
 
-export const keystaticReader = createReader(process.cwd(), keystaticConfig);
+const isProduction = import.meta.env.PROD;
+
+export const keystaticReader = isProduction
+  ? createGitHubReader(keystaticConfig, {
+      repo: 'EmmyStack01/EmmySTACK01',
+      token: import.meta.env.GITHUB_TOKEN,
+    })
+  : createReader(process.cwd(), keystaticConfig);
